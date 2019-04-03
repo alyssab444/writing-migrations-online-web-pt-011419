0d0f322661b33be2136c357e91491a411dbf7696
Alyssa Brown <alyssa.brown@live.com>
2019-04-03 02:02:54 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,7 +2,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change 
     add_column :student, :grade, :integer 
-    add_column :student, :birthdate, :string 
+   
     
   end 
   
